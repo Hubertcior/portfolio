544c6071d37a6cb2c88fcf27ec2d712dc22a0805
Hubertcior <hradziwinski@gmail.com>
2025-07-31 14:34:18 +0200

    init commit

--- src/components/molecules/LinksSection.tsx
+++ src/components/molecules/LinksSection.tsx
@@ -8,7 +8,6 @@ const LinksSection : FC = () => {
         <div className="flex flex-row gap-2">
             <SocialMediaIcon href="https://github.com/Hubertcior" icon={LuGithub} name="github" />
             <SocialMediaIcon href="https://www.linkedin.com/in/hubert-radziwi%C5%84ski-601a89373/" icon={LuLinkedin} colorClass="text-blue-600" name="linkedin"/>
-            {/*TODO: change email when i get my own domain*/}
             <SocialMediaIcon href="mailto:hradziwinski@gmail.com" icon={MdOutlineEmail} name="e-mail" colorClass="text-green-500"/>
         </div>
     )
