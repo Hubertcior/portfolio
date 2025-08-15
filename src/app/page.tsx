de0537139df35d337bffe3bcc5242e41a846d4a2
Hubertcior <hradziwinski@gmail.com>
2025-08-15 19:25:17 +0200

    fix: styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import ExperienceProfile from "@/components/organisms/ExperienceProfile";
 
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen p-8 pb-20 sm:p-20 grid grid-rows-2 gap-16 justify-items-center items-center">
-      <div className="flex flex-col sm:flex-row gap-24 w-full justify-center items-start">
-            <MainProfile />
-          <ExperienceProfile />
+    <div className="font-sans gap-4 min-h-screen pt-2 pb-20 sm:pt-8 sm:pb-20 flex flex-col items-center">
+      <div className="flex flex-col gap-16  w-full justify-center items-center">
+        <MainProfile />
+        <ExperienceProfile />
       </div>
-      <div className="flex flex-col sm:flex-row gap-24 w-full justify-center items-start">
-            <SkillProfile />
-          <EducationProfile />
+      <div className="flex flex-col gap-4 w-full justify-center items-center">
+        <SkillProfile />
+        <EducationProfile />
       </div>
     </div>
   );
--- src/components/atoms/Experience&Education/Logo.tsx
+++ src/components/atoms/Experience&Education/Logo.tsx
@@ -8,13 +8,13 @@ interface LogoProps {
 
 const Logo: FC<LogoProps> = ({ src, alt = "Logo" }) => {
     return (
-        <div className="w-10 h-10 rounded-full overflow-hidden relative bg-white aspect-square">
+        <div className="w-10 h-10 rounded-full overflow-hidden bg-white relative">
             <Image
                 src={src}
                 alt={alt}
                 fill
-                sizes="40px"
                 className="object-contain"
+                sizes="40px"
             />
         </div>
     )
